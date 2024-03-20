865965f04e069f4c5af0eb863363e0f6351ba604
TrAdOri <evjeuland04@gmail.com>
2024-03-20 12:27:57 +0100

    Page admin : Date de parution format

--- src/Entity/Catalogue/Musique.php
+++ src/Entity/Catalogue/Musique.php
@@ -5,24 +5,25 @@ namespace App\Entity\Catalogue;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use DateTime;
 
 #[ORM\Entity]
 class Musique extends Article
 {
-    #[ORM\Column(length: 255,name: 'artiste')]
+    #[ORM\Column(length: 255, name: 'artiste')]
     private ?string $artiste = null;
 
-    #[ORM\Column(length: 255, name: 'date_de_parution')]
-    private ?string $dateDeParution = null;
-	
+    #[ORM\Column(name: 'date_de_parution', type: 'datetime')]
+    private ?DateTime $dateDeParution = null; // Modifier le type ici
+
     #[ORM\OneToMany(targetEntity: "Piste", mappedBy: "musique")]
     private ?Collection $pistes = null;
-	
-	public function __construct()
-	{
-		$this->pistes = new ArrayCollection();
-	}
-	
+
+    public function __construct()
+    {
+        $this->pistes = new ArrayCollection();
+    }
+
     public function getArtiste(): ?string
     {
         return $this->artiste;
@@ -35,32 +36,30 @@ class Musique extends Article
         return $this;
     }
 
-    public function getDateDeParution(): ?string
+    public function getDateDeParution(): ?DateTime // Modifier le type ici
     {
         return $this->dateDeParution;
     }
 
-    public function setDateDeParution(string $dateDeParution): static
+    public function setDateDeParution(DateTime $dateDeParution): static // Modifier le type ici
     {
         $this->dateDeParution = $dateDeParution;
 
         return $this;
     }
-	
+
     public function getPistes(): ?array
     {
-		if($this->pistes == null) {
-			return null ;
-		}
-		else {
-			return $this->pistes->toArray();
-		}
+        if ($this->pistes == null) {
+            return null;
+        } else {
+            return $this->pistes->toArray();
+        }
     }
-	
-	public function addPiste(Piste $piste): void
-	{
-		$piste->setMusique($this);
-		$this->pistes->add($piste);
-	}
-}
 
+    public function addPiste(Piste $piste): void
+    {
+        $piste->setMusique($this);
+        $this->pistes->add($piste);
+    }
+}
