fd6b08ec28bdb0dff666306a33f0bfd8107e02c4
lemoine alex <alexlemoine35@gmail.com>
2024-04-08 15:33:23 +0200

    Mise à jour de la durée de recherche dans AppFixtures.php

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -31,7 +31,7 @@ class AppFixtures extends Fixture
 			//$ebay->setCategory('CDs');
 			$keywords = 'Johnny Hallyday';
 
-			$formattedResponse = $ebay->findItemsAdvanced($keywords, 60);
+			$formattedResponse = $ebay->findItemsAdvanced($keywords, 100);
 			file_put_contents("ebayResponse.xml", $formattedResponse);
 			$xml = simplexml_load_string($formattedResponse);
 
